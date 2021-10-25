83e33d49361f33241ecc087503cab914fd2df3be
divya-hegde4 <92806120+divya-hegde4@users.noreply.github.com>
2021-10-25 18:39:13 -0500

    Update controller.cpp

--- divya/controller.cpp
+++ divya/controller.cpp
@@ -7,19 +7,35 @@ using namespace std;
 #define NUM_SIGNALS 4
 
 int il[NUM_JUNCTIONS][NUM_SIGNALS] = {{0,1,1,1}, {0,1,1,1}, {0,0,1,1}, {0,1,1,1}, {1,1,1,1}, {0,1,1,1}, {0,0,1,1}, {0,1,1,1}, {0,0,1,1}};
+int p[NUM_JUNCTIONS][NUM_SIGNALS] = {{0,1,0,0}, {0,0,1,0}, {0,0,0,1}, {0,1,1,0}, {1,1,0,1}, {0,1,0,0}, {0,0,0,0}, {0,0,0,1}, {0,0,0,0}};
 
-void scheduler (queue <int> q, int jun[]) {
+void scheduler (queue <int> q, int jun[], int priority, int pri[]) {
   int new_light = q.front ();
   q.pop ();
-  if (jun[new_light] != 0) {
-    jun[new_light] = 2; //set to green
-    std::cout << new_light <<"\n";
-    q.push (new_light);
-    return;
+  if (priority == 0) {
+    //simple round robin
+    if (jun[new_light] != 0) {
+      jun[new_light] = 2; //set to green
+      std::cout << new_light <<"\n";
+      q.push (new_light);
+      return;
+    } else {
+      q.push (new_light);
+      scheduler (q, jun, priority, pri);
+    }
   } else {
-    q.push (new_light);
-    scheduler (q, jun);
-  } 
+    //set to green for the signal with priority = 1
+    if (jun[new_light] != 0 && pri[new_light] == 1) {
+      jun[new_light] = 2;
+      std:: cout << "new light from priority " << new_light << "\n";
+      pri[new_light] = 0;
+      q.push (new_light);
+      return;
+    } else {
+      q.push (new_light);
+      scheduler (q, jun, priority, pri);
+    }
+  }
 }
 
 void controller () {
@@ -33,13 +49,17 @@ void controller () {
   //signal is in form of il[i][j]
   for (int i = 0; i < NUM_JUNCTIONS; i++) {
     queue <int> q;
+    int priority = 0;
     for (int j = 0; j < NUM_SIGNALS; j++) {
       if (il[i][j] == 2) {
         il[i][j] = 1; //change the green signals to red
       }
+      if (p[i][j] == 1) {
+        priority = 1;
+      }
       q.push (j);
     }
-    scheduler (q, il[i]);
+    scheduler (q, il[i], priority, p[i]);
   }
 }
 
