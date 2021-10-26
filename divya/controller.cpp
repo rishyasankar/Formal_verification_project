6c0074f4d2d2d172c7676667dfe977656ac9cdf9
divya-hegde4 <92806120+divya-hegde4@users.noreply.github.com>
2021-10-25 19:30:19 -0500

    Some additional checks for priority

--- divya/controller.cpp
+++ divya/controller.cpp
@@ -7,12 +7,18 @@ using namespace std;
 #define NUM_SIGNALS 4
 
 int il[NUM_JUNCTIONS][NUM_SIGNALS] = {{0,1,1,1}, {0,1,1,1}, {0,0,1,1}, {0,1,1,1}, {1,1,1,1}, {0,1,1,1}, {0,0,1,1}, {0,1,1,1}, {0,0,1,1}};
-int p[NUM_JUNCTIONS][NUM_SIGNALS] = {{0,1,0,0}, {0,0,1,0}, {0,0,0,1}, {0,1,1,0}, {1,1,0,1}, {0,1,0,0}, {0,0,0,0}, {0,0,0,1}, {0,0,0,0}};
+int p[NUM_JUNCTIONS][NUM_SIGNALS] = {{0,0,0,0}, {0,0,1,0}, {0,0,0,1}, {0,1,1,0}, {1,1,0,1}, {0,1,0,0}, {0,0,0,0}, {0,0,0,1}, {0,1,0,0}};
 
-void scheduler (queue <int> q, int jun[], int priority, int pri[]) {
+void scheduler (queue <int> q, int jun[], int priority, int pri[], int first) {
   int new_light = q.front ();
   q.pop ();
   if (priority == 0) {
+    //at first junction, always allow cars from node A if no other car is returning back to A
+    if (first == 0) {
+      jun[0] = 2; //change to the signal which faces A
+      std::cout << "0\n";
+      return;
+    }
     //simple round robin
     if (jun[new_light] != 0) {
       jun[new_light] = 2; //set to green
@@ -21,11 +27,17 @@ void scheduler (queue <int> q, int jun[], int priority, int pri[]) {
       return;
     } else {
       q.push (new_light);
-      scheduler (q, jun, priority, pri);
+      scheduler (q, jun, priority, pri, first);
     }
   } else {
+    //if priority = 1 and light is off, change the priority to 0
+    if (jun[new_light] == 0 && pri[new_light] == 1) {
+      priority = 0;
+      q.push (new_light);
+      scheduler (q, jun, priority, pri, first);
+    }
     //set to green for the signal with priority = 1
-    if (jun[new_light] != 0 && pri[new_light] == 1) {
+    else if (jun[new_light] != 0 && pri[new_light] == 1) {
       jun[new_light] = 2;
       std:: cout << "new light from priority " << new_light << "\n";
       pri[new_light] = 0;
@@ -33,7 +45,7 @@ void scheduler (queue <int> q, int jun[], int priority, int pri[]) {
       return;
     } else {
       q.push (new_light);
-      scheduler (q, jun, priority, pri);
+      scheduler (q, jun, priority, pri, first);
     }
   }
 }
@@ -59,7 +71,15 @@ void controller () {
       }
       q.push (j);
     }
-    scheduler (q, il[i], priority, p[i]);
+    scheduler (q, il[i], priority, p[i], i);
+  }
+  // print il[i][j] => to verify only one green light is there at each junction
+  std::cout << "lights\n";
+  for (int i = 0; i < NUM_JUNCTIONS; i++) {
+    for (int j = 0; j < NUM_SIGNALS; j++) {
+      std::cout << il[i][j] << " ";
+    }
+    std::cout << "\n";
   }
 }
 
