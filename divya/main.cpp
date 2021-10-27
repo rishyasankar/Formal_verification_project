0d41640ee317d6406674944c8fe9859f8e2ba3aa
divya-hegde4 <92806120+divya-hegde4@users.noreply.github.com>
2021-10-26 19:25:54 -0500

    Create main.cpp

---
+++ divya/main.cpp
@@ -0,0 +1,69 @@
+#include <iostream>
+using namespace std;
+
+#include "signal.h"
+
+
+int il[NUM_JUNCTIONS][NUM_SIGNALS];
+int p_light[NUM_JUNCTIONS][NUM_SIGNALS];
+int segment[25][30];
+
+int main () {
+  //initialise_queue (q);
+  initial_signal_priority();
+
+  for(int i = 0; i <= 23; i++) {
+    for(int j = 0; j <= 29; j++) {
+      segment[i][j] = 0;
+    }
+  }
+  //segment21
+  segment[21][29]=1;
+  segment[21][27]=4;
+  segment[21][18]=7;
+  segment[21][20]=5;
+
+  //segment22
+  segment[22][29]= 3 ;
+  segment[22][23]= 4;
+  segment[22][25]= 5;
+  segment[22][19]= 6;
+  segment[22][22]= 7;
+  segment[22][12]= 8;
+  segment[22][24]= 0;
+
+  //segment17
+  segment[17][29]= 6 ;
+  segment[17][23]= 9 ;
+  segment[17][28]=12 ;
+
+  //segement18
+  segment[18][29]= 23;
+
+  //segment19
+  segment[19][29]= 27;
+
+  //segment20
+  segment[20][29]= 12;
+  segment[20][28]=13 ;
+  segment[20][27]= 14;
+  segment[20][26]= 15;
+  segment[20][25]= 16;
+  segment[20][24]= 17;
+  segment[20][23]= 18;
+  segment[20][22]= 19;
+
+  //segment23
+  segment[23][29]= 20;
+
+  //segment16
+  segment[16][29]= 34;
+  segment[16][18]= 35;
+
+  
+  priority_signal();
+
+  controller ();
+  return 0;
+}
+
