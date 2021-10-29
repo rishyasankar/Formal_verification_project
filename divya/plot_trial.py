2469d7a8304f240e7bab3da48aec345f4d72c290
divya-hegde4 <92806120+divya-hegde4@users.noreply.github.com>
2021-10-29 16:42:12 -0500

    Update plot_trial.py

--- divya/plot_trial.py
+++ divya/plot_trial.py
@@ -1,34 +1,37 @@
 import matplotlib.pyplot as plt
-import numpy as np
 
-data = [[0,1,2,1], [0,1,1,1], [0,0,1,1], [0,1,1,1], [1,1,1,1], [0,1,1,1], [0,0,1,1], [0,1,1,1], [0,0,1,1]]
+# read data from file : for each row : 4 lights
+# each light will have a particular point assigned
+# and based on the value colour changes
+# 0 = black, 1 = red and 2 = green
+# repeat for each row
 
-fig, ax = plt.subplots()
-
-data_array = np.array(data)
-
-"""
-for i in data_array:
-  for j in i:
-    if j != 0:
-      if j == 1:
-        ax.plot(i, j, marker='x', linestyle='', ms=12, color='red')
-      else:
-        ax.plot(i, j, marker='o', linestyle='', ms=12, color='green')
-"""
-print(data_array)
-print(data)
-
-for i in data_array:
-  print (i)
-  for j in i:
-    if j != 0:
-      if j == 1:
-        ax.plot(0, 0, marker='x', linestyle='', ms=12, color='red')
-      else:
-        ax.plot(0, 0, marker='o', linestyle='', ms=12, color='green')
+with open ("outputfile", 'r') as f:
+  lines = f.readlines ()
 
 
-plt.ylim (-1, 4)
-plt.xlim (-1, 4)
+x = [[1,1.5,1,0.5]]
+y = [[61.5,61,60.5,61]]
+
+il = [[1,2,0,1], [2,1,0,1], [2,1,0,0], [1,0,1,2], [1,1,2,1], [1,2,1,0], [0,0,2,1], [0,1,2,1], [0,2,1,0]]
+
+for i in range(1):
+  print(il[i]) 
+  for j in range(len(il[i])):
+    print(j)
+    if il[i][j] == 1:
+      colour = "red"
+    elif il[i][j] == 2:
+      colour = "green"
+    else:
+      colour = "black"
+    xi = x[i][j]
+    yi = y[i][j]
+    plt.scatter (xi, yi, color = colour)
+
+
+    
+
+
+
 plt.show ()
