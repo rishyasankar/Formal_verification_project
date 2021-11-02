88d207c77d350404adf0b0af6c048a4e6a52dd28
divya-hegde4 <92806120+divya-hegde4@users.noreply.github.com>
2021-11-01 22:38:34 -0500

    Create trafficlights.h

---
+++ divya/trafficlights.h
@@ -0,0 +1,34 @@
+#include <queue.h>
+
+il[9][4] = {0};
+
+void initialise_queue () {
+  for (int i = 0; i < 9; i++) {
+    for (int j = 0; j < 4; j++) {
+      q[i].push (j);
+    }
+  }
+}
+
+void switch_light () {
+  for (int i = 0; i < 9; i++) {
+    for (int j = 0; j < 4; j++) {
+      if (il[i][j] == 2) {
+        il[i][j] = 1;
+      }
+    }
+    int new_light = q[i].front ();
+    q[i].pop ();
+    il[i][new_light] = 2;
+    q[i].push (new_light);
+  }
+}
+
+int main () {
+  int time = 0;
+  initialise_queue ();
+  while (time < 3600) {
+    switch_light ();
+    time = time + 2;
+  }
+}
